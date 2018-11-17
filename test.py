ceb5bc8d26bced4b5ec99ca618f834ad54ce0538
armiro <arman.hgh.1374@gmail.com>
2018-11-17 13:33:40 +0330

    test file

--- test.py
+++ test.py
@@ -1,37 +1,15 @@
-import math
-import numpy as np
-# import scipy as sp
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-# from scipy.special import ellipk
-# from sklearn.feature_extraction import image
-# from sklearn.cluster import spectral_clustering
-from skimage import io, filters
-from skimage.color import rgb2gray
-from skimage.segmentation import active_contour
 
-path = "./test-images/female_27.bmp"
-img = io.imread(path)
-img = rgb2gray(img)
-# img = filters.frangi(img, scale_range=(1, 15), scale_step=2,
-#                      beta1=0.5, beta2=15, black_ridges=True)
-io.imshow(img)
-io.show()
+def CLAHE(image):
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl = clahe.apply(image)
+    return cl
 
-# using Snake active contour for segmentation
-s = np.linspace(0, 2*math.pi, 100)
-x = img.shape[1]/2 + 180*np.sin(s)
-y = img.shape[0]/2 + 180*np.cos(s)
-init = np.array([x, y]).T
 
-# snake = active_contour(img, init, bc='periodic',
-#                        alpha=0.015, beta=10, gamma=0.001)
-#
-# fig, ax = plt.subplots(figsize=(7, 7))
-# ax.imshow(img, cmap=plt.cm.gray)
-# ax.plot(init[:, 0], init[:, 1], '--r', lw=3)
-# ax.plot(snake[:, 0], snake[:, 1], '-b', lw=2)
-# ax.set_xticks([]), ax.set_yticks([])
-# ax.axis([0, img.shape[1], img.shape[0], 0])
-# plt.show()
+img_address = "./test-images/female_35.bmp"
+img = cv2.imread(img_address, 0)
+adaptive_equalized = CLAHE(image=img)
 
