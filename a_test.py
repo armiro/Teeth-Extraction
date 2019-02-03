145918e09539ac8ed4502489b4ffb6abe2f75b9e
armiro <arman.hgh.1374@gmail.com>
2019-02-03 16:39:52 +0330

    initial roi extraction/revision check successful!

--- a_test.py
+++ a_test.py
@@ -2,8 +2,15 @@ import cv2
 import preprocessing
 import matplotlib.pyplot as plt
 import numpy as np
-from ROI_revision import revise_vertical_boundaries
+from ROI_revision import *
+from ROI_extraction import *
 
-img_address = './test-revised/1.bmp'
+img_address = './test-images/2.bmp'
 img = cv2.imread(img_address, 0)
-revise_vertical_boundaries(image=img, show_result=True, return_result=False)
+
+initial_roi = extract_roi(image=img, return_result=1)
+final_roi = revise_boundaries(image=initial_roi, return_result=1)
+
+plt.subplot(2, 1, 1), plt.imshow(X=img, cmap='gray')
+plt.subplot(2, 1, 2), plt.imshow(X=final_roi, cmap='gray')
+plt.show()
