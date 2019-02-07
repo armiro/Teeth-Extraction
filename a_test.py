c05e6fa57aa4eac9d50ce3095d472a746892e6dd
armiro <arman.hgh.1374@gmail.com>
2019-02-07 20:01:36 +0330

    roi extraction/ revision on 50 images

--- a_test.py
+++ a_test.py
@@ -4,30 +4,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 from ROI_revision import *
 from ROI_extraction import *
+from tooth_extraction import draw_middle_line
 
-img_address = './test-images/2.bmp'
-img = cv2.imread(img_address, 0)
-print('original image dimensions:', img.shape)
-initial_roi, initial_boundaries = extract_roi(image=img, return_result=1)
-print('initial ROI dimensions:', initial_roi.shape)
-revised_roi, revised_boundaries = revise_boundaries(image=initial_roi, return_result=1)
-print('final ROI dimensions:', revised_roi.shape)
+for i in range(1, 51):
+    img_address = './images/%d.bmp' % i
+    img = cv2.imread(img_address, 0)
+    print('original image dimensions:', img.shape)
+    initial_roi, initial_boundaries = extract_roi(image=img, return_result=1)
+    print('initial ROI dimensions:', initial_roi.shape)
+    revised_roi, revised_boundaries = revise_boundaries(image=initial_roi, return_result=1)
+    print('final ROI dimensions:', revised_roi.shape)
+    revised_roi = draw_middle_line(image=revised_roi, num_parts=60, show_result=False, return_result=True)
 
-# print(initial_boundaries)
-# print(final_boundaries)
+    # print(initial_boundaries)
+    # print(final_boundaries)
 
-upper_height = initial_boundaries[3] + revised_boundaries[3]
-left_width = initial_boundaries[0] + revised_boundaries[0]
-lower_height = img.shape[0] - initial_boundaries[2] + revised_boundaries[2]
-right_width = img.shape[1] - initial_boundaries[1] + revised_boundaries[1]
+    upper_height = initial_boundaries[3] + revised_boundaries[3]
+    left_width = initial_boundaries[0] + revised_boundaries[0]
+    # lower_height = img.shape[0] - initial_boundaries[2] + revised_boundaries[2]
+    lower_height = upper_height + revised_roi.shape[0]
+    right_width = left_width + revised_roi.shape[1]
+    # right_width = initial_boundaries[1]
 
-up_left_corner = (upper_height, left_width)
-up_right_corner = (upper_height, right_width)
-down_left_corner = (lower_height, left_width)
-down_right_corner = (lower_height, right_width)
+    top_left_corner = (left_width, upper_height)
+    top_right_corner = (right_width, upper_height)
+    bottom_left_corner = (left_width, lower_height)
+    bottom_right_corner = (right_width, lower_height)
 
-print('roi points:', up_left_corner, up_right_corner, down_left_corner, down_right_corner)
+    print('roi points:', top_left_corner, top_right_corner, bottom_left_corner, bottom_right_corner)
 
-plt.subplot(2, 1, 1), plt.imshow(X=img, cmap='gray')
-plt.subplot(2, 1, 2), plt.imshow(X=revised_roi, cmap='gray')
-plt.show()
+    cv2.rectangle(img, top_left_corner, bottom_right_corner, 0, 7)
+    # cv2.circle(img, top_left_corner, 20, 1, -1)
+    # cv2.circle(img, top_right_corner, 20, 1, -1)
+    # cv2.circle(img, bottom_left_corner, 20, 1, -1)
+    # cv2.circle(img, bottom_right_corner, 20, 1, -1)
+
+    fig = plt.figure()
+    plt.subplot(2, 1, 1), plt.imshow(X=img, cmap='gray')
+    plt.subplot(2, 1, 2), plt.imshow(X=revised_roi, cmap='gray')
+    plt.show()
+
+    file_name = './cropped-figures/%d' % i
+    fig.savefig(file_name)
+
+    # cv2.imwrite('test-auto-cropped/10.bmp', revised_roi)
