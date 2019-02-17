8fe1c30eae1dd0937e6056b7f90f7eabd05df835
armiro <arman.hgh.1374@gmail.com>
2019-02-17 11:46:40 +0330

    add comments

--- user_defined_roi_corners.py
+++ user_defined_roi_corners.py
@@ -5,7 +5,7 @@ import csv
 
 csv_header = ['image_number', 'top_left_corner', 'bottom_right_corner']
 csv_path = "./real_roi_corners.csv"
-this_roi_corner_points = list()
+roi_corner_points = list()
 
 
 def initialize_csv_reader(path):
@@ -21,25 +21,31 @@ def append_to_csv(record, csv_file, writer):
 
 
 def draw_rectangle(event, x, y, flags, param):
-    global this_roi_corner_points
+    global roi_corner_points
     this_img = copy.deepcopy(x=img)
 
+    # when left-click pressed, record starting (x, y)
     if event == cv2.EVENT_LBUTTONDOWN:
-        this_roi_corner_points = [(x, y)]
+        roi_corner_points = [(x, y)]
+
+    # when left-click hanged-ff, record ending (x, y) and draw a rectangle
+    # from the starting point to the ending point
     elif event == cv2.EVENT_LBUTTONUP:
-        this_roi_corner_points.append((x, y))
-        cv2.rectangle(this_img, this_roi_corner_points[0], this_roi_corner_points[1], 255, 3)
+        roi_corner_points.append((x, y))
+        cv2.rectangle(this_img, roi_corner_points[0], roi_corner_points[1], 255, 3)
         cv2.imshow('image number %d' % i, this_img)
 
-    # confirm the ROI with right-click
+    # confirm the ROI points with right-click
     if event == cv2.EVENT_RBUTTONDOWN:
         cv2.destroyWindow('image number %d' % i)
-        print("roi corner points of image %d are:" % i, this_roi_corner_points)
-        this_record = [i, this_roi_corner_points[0], this_roi_corner_points[1]]
+        print("roi corner points of image %d are:" % i, roi_corner_points)
+        this_record = [i, roi_corner_points[0], roi_corner_points[1]]
         append_to_csv(record=this_record, csv_file=csv_file, writer=writer)
 
 
 writer, csv_file = initialize_csv_reader(path=csv_path)
+
+# for each image, save the user-defined roi
 for i in range(1, 51):
     img = cv2.imread('./images/%d.bmp' % i, 0)
     cv2.namedWindow('image number %d' % i, cv2.WINDOW_FREERATIO)
