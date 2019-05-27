dd20961fec3fc12b49eea1910b68cc2b9914fa96
armiro <arman.hgh.1374@gmail.com>
2019-05-28 02:24:14 +0430

    modularize main function

--- jaw_separation.py
+++ jaw_separation.py
@@ -1,48 +1,37 @@
-import cv2
+# import cv2
 import preprocessing as prep
-import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 
-img = cv2.imread('./test-images/snaked_7_sauvola.bmp', 0)
-height, width = img.shape[:2]
-print('image shape:', img.shape)
+def separate_jaws(image):
+    height, width = image.shape[:2]
+    upper_jaw = np.zeros(shape=(height, 1))
+    lower_jaw = np.zeros(shape=(height, 1))
+    line_points = list()
 
-upper_jaw = np.zeros(shape=(height, 1))
-lower_jaw = np.zeros(shape=(height, 1))
-line_points = list()
+    # change pixels with the value of 0 to 1; for further purpose of applying genetic algorithm
+    image = prep.eliminate_black_pixels(image=image)
+    middle_line_pixels = list()
 
-# change pixels with the value of 0 to 1; for further purpose of applying genetic algorithm
-img = prep.eliminate_black_pixels(image=img)
-middle_line_pixels = list()
+    for column in range(0, width):
+        this_column = image[:, column]
+        for px_idx in range(0, len(this_column)):
+            if this_column[px_idx] == 255:
+                line_points.append([column, px_idx])
+                up = np.vstack(tup=(image[:px_idx, column:column+1], np.zeros(shape=(height-px_idx, 1))))
+                upper_jaw = np.hstack(tup=(upper_jaw, up))
 
-t0 = time.time()
-for column in range(0, width):
-    this_column = img[:, column]
-    for px_idx in range(0, len(this_column)):
-        if this_column[px_idx] == 255:
-            line_points.append([column, px_idx])
-            up = np.vstack(tup=(img[:px_idx, column:column+1], np.zeros(shape=(height-px_idx, 1))))
-            upper_jaw = np.hstack(tup=(upper_jaw, up))
+                dn = np.vstack(tup=(np.zeros(shape=(px_idx+1, 1)), image[px_idx+1:, column:column+1]))
+                lower_jaw = np.hstack(tup=(lower_jaw, dn))
 
-            dn = np.vstack(tup=(np.zeros(shape=(px_idx+1, 1)), img[px_idx+1:, column:column+1]))
-            lower_jaw = np.hstack(tup=(lower_jaw, dn))
+                middle_line_pixels.append(px_idx)
+                break
 
-            middle_line_pixels.append(px_idx)
-            break
-t1 = time.time()
-print('elapsed time for jaw separation: %.2f secs' % round(t1-t0, 2))
+    upper_bound = max(middle_line_pixels)
+    upper_jaw = upper_jaw[:upper_bound + 1, :]
 
-upper_bound = max(middle_line_pixels)
-upper_jaw = upper_jaw[:upper_bound + 1, :]
-plt.imshow(X=upper_jaw, cmap='gray')
-plt.show()
-cv2.imwrite('./test-images/7_upper_sauvola.bmp', upper_jaw)
+    lower_bound = min(middle_line_pixels)
+    lower_jaw = lower_jaw[lower_bound + 1:, :]
 
-lower_bound = min(middle_line_pixels)
-lower_jaw = lower_jaw[lower_bound + 1:, :]
-plt.imshow(X=lower_jaw, cmap='gray')
-plt.show()
-cv2.imwrite('./test-images/7_lower_sauvola.bmp', lower_jaw)
+    return upper_jaw, lower_jaw
 
